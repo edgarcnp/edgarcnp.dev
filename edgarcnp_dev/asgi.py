1bd0f64390a252e201d3b21798c4b315da4868e5
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-09 22:21:05 +0700

    fix lint

--- edgarcnp_dev/asgi.py
+++ edgarcnp_dev/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edgarcnp_dev.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edgarcnp_dev.settings")
 
 application = get_asgi_application()
