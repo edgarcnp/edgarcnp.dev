77f77abcbd88f5e4dbc2c94d4173659ad5feefc7
Edgar Christian <edgarchristian2000@gmail.com>
2023-03-16 23:11:02 +0700

    fix lint

--- base/views.py
+++ base/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        user_ip_address = x_forwarded_for.split(',')[0]
+        user_ip_address = x_forwarded_for.split(",")[0]
     else:
-        user_ip_address = request.META.get('REMOTE_ADDR')
+        user_ip_address = request.META.get("REMOTE_ADDR")
 
     respond = {"user_ip_address": user_ip_address}
     return render(request, "base_home.html", respond)
