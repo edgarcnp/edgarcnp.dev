c0885a9e0e92817ccaee33f99c46f255a5134e5b
Edgar Christian <me@edgarcnp.dev>
2026-06-20 03:52:54 +0700

    feat: add cloudflare stuff

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     plugins: [
         solidStart(),
         tailwindcss(),
-        nitro()
+        nitro({
+            preset: "cloudflare-module"
+        })
     ]
 });
